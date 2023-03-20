bbdf53d23f3141dbad3c7ac2da3bf160a53575a5
siriusdevs <tenigin.aa@talantiuspeh.ru>
2023-03-20 15:37:05 +0300

    uuid default v4

--- refrigerator/fridge/models.py
+++ refrigerator/fridge/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from uuid import uuid4
 
 
 class UUIDMixin(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid4)
 
     class Meta:
         abstract = True
